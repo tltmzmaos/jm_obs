10f00e631c8c640ebfee889e2fd55ea1352234d6
jongminlee <tltmzmaos@gmail.com>
2024-04-07 22:30:09 -0700

    Quartz sync: Apr 7, 2024, 10:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jongmin.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
